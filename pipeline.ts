ae8b2e1af85b0dbd4273aa7b44d50a881280fcf5
PatrickJS <github@patrickjs.com>
2026-06-21 23:49:44 -0700

    ci(flow): use NPM_TOKEN for npm publish

--- pipeline.ts
+++ pipeline.ts
@@ -164,7 +164,7 @@ export default definePipeline({
       },
       env: {
         GITHUB_TOKEN: env.secret("GITHUB_TOKEN"),
-        NODE_AUTH_TOKEN: env.secret("npm_token")
+        NODE_AUTH_TOKEN: env.secret("NPM_TOKEN")
       },
       github: {
         permissions: {
